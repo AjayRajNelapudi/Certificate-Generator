72321c7a51c4b208fd75e676a1187ecd566bde57
AjayRajNelapudi <najayraj.16.cse@anits.edu.in>
2020-01-07 21:16:44 +0530

    setup and requirements added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Certificate Generator',
+    version='1.0',
+    description='Generates and emails certificates',
+    author='Ajay Raj Nelapudi',
+    author_email='ajayraj.cseanits@gmail.com',
+    url='https://ajayrajnelapudi.github.io/',
+    install_requires = [
+        'opencv-python==3.3.0.10',
+        'img2pdf==0.3.3',
+        'tkinter==8.5',
+    ]
+)

997aa7383acecb2eb84e197ecf215fcd628fe658
AjayRajNelapudi <najayraj.16.cse@anits.edu.in>
2020-01-08 20:39:08 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Certificate Generator',
-    version='1.0',
+    version='2.0',
     description='Generates and emails certificates',
     author='Ajay Raj Nelapudi',
     author_email='ajayraj.cseanits@gmail.com',
